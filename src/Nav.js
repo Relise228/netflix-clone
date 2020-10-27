927221c73074e237fd166da0d3d8fa0e89e8c9bb
Relise228 <andreysmit43@gmail.com>
2020-10-27 15:47:36 +0200

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+import './Nav.css';
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+      <img
+        className='nav__logo'
+        src='https://logos-download.com/wp-content/uploads/2016/03/Netflix_logo.png'
+        alt='Netflix Logo'
+      />
+
+      <img
+        className='nav__avatar'
+        src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png'
+        alt='Netflix Logo'
+      />
+    </div>
+  );
+}
+
+export default Nav;
